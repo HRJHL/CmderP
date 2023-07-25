6c2c17cfe7d772ba689cc49835e3712e81010fe8
HRJHL <jinho99kr30@gmail.com>
2023-07-25 17:07:26 +0900

    CBV로 블로그 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,10 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+class PostDetail(DetailView):
+    model = Post
+
+
 #def index(request):
 #    posts = Post.objects.all().order_by('-pk')
     #return render(
@@ -16,13 +20,13 @@ class PostList(ListView):
  #       }
 #    )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_page.html',
-        {
-            'post': post,
-        }
-    )
+#def single_post_page(request, pk):
+#    post = Post.objects.get(pk=pk)
+#
+ #   return render(
+  #      request,
+   #     'blog/post_detail.html',
+    #    {
+     #       'post': post,
+      #  }
+    #)
