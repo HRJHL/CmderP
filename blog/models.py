84f72ec7dd3093eb34ebc4d244bf045a53ebd4c7
HRJHL <jinho99kr30@gmail.com>
2023-07-25 13:36:37 +0900

    모델을 생서하고, 시간설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30) #짧은 내용 입력
+    content = models.TextField() #긴내용 입력
+    #작성 시간
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
