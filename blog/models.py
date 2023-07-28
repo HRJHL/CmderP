9aceb1488056c7dc33acf2ea4a9d2da11a0be1e1
HRJHL <jinho99kr30@gmail.com>
2023-07-28 15:47:21 +0900

    15차시 카테고리 태그 완료

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,36 @@ from django.contrib.auth.models import User
 import os
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
+
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
@@ -15,8 +41,11 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
+
     def __str__(self):
-        return f'[{self.pk}] {self.title} :: {self.author}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
 #    path('<int:pk>/', views.single_post_page),
 #    path('', views.index),
-    path('<int:pk>/',views.PostDetail.as_view()),
+    path('tag/<str:slug>/', views.tag_page),
+    path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
